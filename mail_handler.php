0f817aa4e474e908e59cfd0b4edb6b8aa7b06dae
Fernify <dtfern@gmail.com>
2015-04-06 02:13:58 +0100

    Style Consolidation and Mobile Cleanup

--- mail_handler.php
+++ mail_handler.php
@@ -3,8 +3,9 @@ if(isset($_POST['submit'])){
     $to = "contact@fernify.co.uk"; // this is your Email address
     $from = stripcslashes($_POST['emailText']);
     $name = stripcslashes($_POST['nameText']);
-    $subject = "Form submission";
-    $message = stripcslashes($name . " " . $_POST['message']);
+    $subject = "An email has been sent by ".$name;
+    $message = stripcslashes($_POST['messageText']);
     mail($to,$subject,$message,"");
     }
+    header("Location: only.html");
 ?>
