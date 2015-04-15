99259e64d0160716e09325d5d1af1484ef579190
Fernify <dtfern@gmail.com>
2015-04-15 20:31:08 +0100

    Full PHP Ajax

--- mail_handler.php
+++ mail_handler.php
@@ -1,8 +1,11 @@
 <?php 
-    $to = "contact@fernify.co.uk";
-    $from = stripcslashes($_REQUEST['e']);
-    $name = stripcslashes($_REQUEST['n']);
+if(isset($_POST['submit'])){
+    $to = "contact@fernify.co.uk"; // this is your Email address
+    $from = stripcslashes($_POST['emailText']);
+    $name = stripcslashes($_POST['nameText']);
     $subject = "An email has been sent by ".$name;
-    $message = stripcslashes($_REQUEST['m']);
+    $message = stripcslashes($_POST['messageText']);
     mail($to,$subject,$message,"");
+    }
+    header("Location: only.html");
 ?>
