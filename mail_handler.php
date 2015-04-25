a3505b23c164f9a68ded4b1cc8d2cb9a361325a2
Fernify <dtfern@gmail.com>
2015-04-25 15:32:43 +0100

    Ajax Email Fix

--- mail_handler.php
+++ mail_handler.php
@@ -1,11 +1,10 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "contact@fernify.co.uk"; // this is your Email address
-    $from = stripcslashes($_POST['emailText']);
-    $name = stripcslashes($_POST['nameText']);
-    $subject = "An email has been sent by ".$name;
-    $message = stripcslashes($_POST['messageText']);
-    mail($to,$subject,$message,"");
+	    $to = "contact@fernify.co.uk"; // this is your Email address
+	    $from = stripcslashes($_POST['emailText']);
+	    $name = stripcslashes($_POST['nameText']);
+	    $subject = "An email has been sent by ".$name;
+	    $message = stripcslashes($_POST['messageText']);
+	    mail($to,$subject,$message);
     }
-    header("Location: only.html");
 ?>
