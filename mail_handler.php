5441c287097ec5b1cda9cba450c978e371a0a5e6
Fernify <dtfern@gmail.com>
2015-04-26 14:41:17 +0100

    Carousel Fix

--- mail_handler.php
+++ mail_handler.php
@@ -3,8 +3,8 @@ if(isset($_POST['submit'])){
 	    $to = "contact@fernify.co.uk"; // this is your Email address
 	    $from = stripcslashes($_POST['emailText']);
 	    $name = stripcslashes($_POST['nameText']);
-	    $subject = "An email has been sent by ".$name;
-	    $message = stripcslashes($_POST['messageText']);
+	    $subject = "An email has been sent by ".$from;
+	    $message = $name." sends the message: ".stripcslashes($_POST['messageText']);
 	    mail($to,$subject,$message);
     }
 ?>
