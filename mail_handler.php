753c7a9fc9fcf842dfd7e9929a626962134bcf37
Fernify <dtfern@gmail.com>
2015-04-26 14:28:57 +0100

    Missing Ajax, Carousel Fixed

--- mail_handler.php
+++ mail_handler.php
@@ -1,8 +1,10 @@
 <?php 
-    $to = "contact@fernify.co.uk";
-    $from = stripcslashes($_REQUEST['e']);
-    $name = stripcslashes($_REQUEST['n']);
-    $subject = "An email has been sent by ".$name;
-    $message = stripcslashes($_REQUEST['m']);
-    mail($to,$subject,$message,"");
+if(isset($_POST['submit'])){
+	    $to = "contact@fernify.co.uk"; // this is your Email address
+	    $from = stripcslashes($_POST['emailText']);
+	    $name = stripcslashes($_POST['nameText']);
+	    $subject = "An email has been sent by ".$from;
+	    $message = $name." sends the message: ".stripcslashes($_POST['messageText']);
+	    mail($to,$subject,$message);
+    }
 ?>
