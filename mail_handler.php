40c380da9c20cdea7073555b9b89ee92ae19134b
Fernify <dtfern@gmail.com>
2015-04-29 21:23:22 +0100

    Ajax and CarouselFix

--- mail_handler.php
+++ mail_handler.php
@@ -1,10 +1,10 @@
 <?php 
-if(isset($_POST['submit'])){
-	    $to = "contact@fernify.co.uk"; // this is your Email address
-	    $from = stripcslashes($_POST['emailText']);
-	    $name = stripcslashes($_POST['nameText']);
-	    $subject = "An email has been sent by ".$from;
-	    $message = $name." sends the message: ".stripcslashes($_POST['messageText']);
-	    mail($to,$subject,$message);
-    }
+
+$to = "contact@fernify.co.uk"; // this is your Email address
+$from = stripcslashes($_POST['e']);
+$name = stripcslashes($_POST['n']);
+$subject = "An email has been sent by ".$from;
+$message = $name." sends the message: ".stripcslashes($_POST['m']);
+mail($to,$subject,$message);
+    
 ?>
